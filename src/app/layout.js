f8931bd4e21525dd065af7bc9838968a03a41f30
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-26 16:19:39 +0100

    Refactor layout.js: Remove unnecessary meta tags and add suppressHydrationWarning attribute

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,25 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="fr">
-			<head>
-				<meta
-					httpEquiv="Content-Type"
-					content="text/html; charset=utf-8"
-				/>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, shrink-to-fit=no"
-				/>
-				<meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-				<meta name="description" content={metadata.description} />
-				<meta property="og:type" content="website" />
-				<title>{metadata.title}</title>
+			<body className={inter.className} suppressHydrationWarning={true}>
 				<link
 					rel="stylesheet"
 					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 				/>
-			</head>
-			<body className={inter.className}>
 				<Header />
 				{children}
 			</body>
